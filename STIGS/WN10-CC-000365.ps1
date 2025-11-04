f4686f79596093765d7b2a48ffc78415668490a4
Drew Howard <drewcaelanhoward@gmail.com>
2025-11-04 15:52:45 -0500

    Update WN10-CC-000365.ps1

--- STIGS/WN10-CC-000365.ps1
+++ STIGS/WN10-CC-000365.ps1
@@ -10,7 +10,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-AU-000500
+    STIG-ID         : WN10-CC-000365
 
 .TESTED ON
     Date(s) Tested  : 
