e67e5988fef9c3e964fa18d8a2a750ef052bd203
Drew Howard <drewcaelanhoward@gmail.com>
2025-11-04 13:09:20 -0500

    WN10-AU-000500.ps1

---
+++ STIGS/WN10-AU-000500.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Drew Howard
+    GitHub          : github.com/drewcaelanhoward
+    Date Created    : 2025-11-04
+    Last Modified   : 2025-11-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
+$path = 'HKLM: \SOFTWARE\Policies\Microsoft\Windows\EvnetLog\Applicatoin'
+
+$name = 'MaxSize'
+$value = 32768KB = 33554432
+
+New-Item -Path $path -Force | Out-Null
+New-ItemProperty -Path $path -Name $name
+-Value $value -Propery -Path $path -Name $name -Value $value -PropertyType DWord -Force \ Out-Null
+Write-Host "Set $name to $value bytes (32 MB) under $path"
